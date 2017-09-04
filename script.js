a5ba4381181fb7146135d56abf160eb772f4d8ed
longto <long.to@niteco.se>
2017-09-04 20:20:41 +0700

    init commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+var config = {
+    apiKey: "AIzaSyCSU5HodO7sSPEOePaTOK2N7I76ly3M2_4",
+    authDomain: "fire-chat-3fb89.firebaseapp.com",
+    databaseURL: "https://fire-chat-3fb89.firebaseio.com",
+    projectId: "fire-chat-3fb89",
+    storageBucket: "fire-chat-3fb89.appspot.com",
+    messagingSenderId: "395021246148"
+};
+firebase.initializeApp(config);
